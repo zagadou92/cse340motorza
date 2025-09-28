b264b7fbfa36451f057f065a26d36544bdca9f17
zagadou <86722630+zagadou92@users.noreply.github.com>
2025-09-28 15:38:40 -0700

    Update inventory.js

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -1,44 +1,63 @@
 "use strict";
 
-// Get a list of items in inventory based on the classification_id
-let classificationList = document.querySelector("#classificationList");
-classificationList.addEventListener("change", function () {
-  let classification_id = classificationList.value;
-  console.log(`classification_id is: ${classification_id}`);
-  let classIdURL = "/inv/getInventory/" + classification_id;
-  fetch(classIdURL)
-    .then(function (response) {
-      if (response.ok) {
-        return response.json();
-      }
-      throw Error("Network response was not OK");
-    })
-    .then(function (data) {
-      console.log(data);
-      buildInventoryList(data);
-    })
-    .catch(function (error) {
-      console.log("There was a problem: ", error.message);
-    });
-});
+document.addEventListener("DOMContentLoaded", () => {
+  const classificationList = document.querySelector("#classificationList");
+  const inventoryDisplay = document.getElementById("inventoryDisplay");
 
-// Build inventory items into HTML table components and inject into DOM 
-function buildInventoryList(data) { 
- let inventoryDisplay = document.getElementById("inventoryDisplay"); 
- // Set up the table labels 
- let dataTable = '<thead>'; 
- dataTable += '<tr><th>Vehicle Name</th><td>&nbsp;</td><td>&nbsp;</td></tr>'; 
- dataTable += '</thead>'; 
- // Set up the table body 
- dataTable += '<tbody>'; 
- // Iterate over all vehicles in the array and put each in a row 
- data.forEach(function (element) { 
-  console.log(element.inv_id + ", " + element.inv_model); 
-  dataTable += `<tr><td>${element.inv_make} ${element.inv_model}</td>`; 
-  dataTable += `<td><a href='/inv/edit/${element.inv_id}' title='Click to update'>Modify</a></td>`; 
-  dataTable += `<td><a href='/inv/delete/${element.inv_id}' title='Click to delete'>Delete</a></td></tr>`; 
- }) 
- dataTable += '</tbody>'; 
- // Display the contents in the Inventory Management view 
- inventoryDisplay.innerHTML = dataTable; 
-}
+  if (!classificationList || !inventoryDisplay) return;
+
+  // Écoute le changement de classification
+  classificationList.addEventListener("change", async () => {
+    const classification_id = classificationList.value;
+
+    // Si aucune classification n'est sélectionnée, vide le tableau
+    if (!classification_id) {
+      inventoryDisplay.innerHTML = "<tr><td colspan='3'>Please select a classification</td></tr>";
+      return;
+    }
+
+    try {
+      console.log(`Fetching vehicles for classification_id: ${classification_id}`);
+      const response = await fetch(`/inv/getInventory/${classification_id}`);
+      if (!response.ok) throw new Error("Network response was not OK");
+
+      const data = await response.json();
+      buildInventoryTable(data);
+    } catch (error) {
+      console.error("There was a problem fetching the inventory:", error);
+      inventoryDisplay.innerHTML = `<tr><td colspan="3">Failed to load inventory data.</td></tr>`;
+    }
+  });
+
+  // Fonction pour construire le tableau HTML des véhicules
+  function buildInventoryTable(data) {
+    if (!data || data.length === 0) {
+      inventoryDisplay.innerHTML = "<tr><td colspan='3'>No vehicles found for this classification.</td></tr>";
+      return;
+    }
+
+    let tableHTML = `
+      <thead>
+        <tr>
+          <th>Vehicle Name</th>
+          <th>Modify</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+    `;
+
+    data.forEach((vehicle) => {
+      tableHTML += `
+        <tr>
+          <td>${vehicle.inv_make} ${vehicle.inv_model}</td>
+          <td><a href="/inv/edit/${vehicle.inv_id}" title="Click to update">Modify</a></td>
+          <td><a href="/inv/delete/${vehicle.inv_id}" title="Click to delete">Delete</a></td>
+        </tr>
+      `;
+    });
+
+    tableHTML += "</tbody>";
+    inventoryDisplay.innerHTML = tableHTML;
+  }
+});
